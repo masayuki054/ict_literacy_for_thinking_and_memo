7b486161ff52e3e1700ec0b948b1a06af13867a9
sss <sss@mini-2.local>
2024-04-16 07:18:20 +0900

    push test 2024.04.16

--- org/site.sh
+++ org/site.sh
@@ -1,1 +1,10 @@
-cp ./index.html
+# htmls = index.html story.html docs.html link.html
+# folders = Maps/ docs/ i_maps/
+
+cp index.html docs.html link.html memo.html ../docs/
+cp index.html ../docs/story.html
+mkdir -p ../docs/Maps/ ../docs/docs/ ../docs/i_maps/
+
+cp -Rp Maps/ ../docs/Maps/
+cp -Rp docs/ ../docs/docs/
+cp -Rp i_maps/ ../docs/i_maps/
